708811068f51764b4a8bc783933183412ca29f93
EricKappy.ai <eric.kapilik@sanctuary.ai>
2024-12-17 13:46:09 -0800

    for c/cpp files add column line to visualize line limit

--- nvim/.config/nvim/lua/user/vim.lua
+++ nvim/.config/nvim/lua/user/vim.lua
@@ -1,7 +1,21 @@
 vim.o.number = true
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#A9A9A9" }) -- Light gray for line numbers
 
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 
 vim.g.mapleader = " "
+
+-- Add column line at 80 characters
+vim.api.nvim_create_augroup("C_Cpp_Settings", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = "C_Cpp_Settings",
+  pattern = { "c", "cpp", "h", "hpp" },
+  callback = function()
+    vim.opt_local.colorcolumn = "81"
+    vim.opt_local.textwidth = 80
+    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#D3D3D3" })
+  end,
+})
